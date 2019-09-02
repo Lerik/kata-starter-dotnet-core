3973e987e7f6fc7c646089ef600574b0cc63d7a0
Jacob Ramirez <mr.jacob.ramirez@gmail.com>
2019-09-02 14:27:16 -0500

    Exercise 1, scenario 1 passed, scenario 2 failed

---
+++ src/Kata.Spec/when_adding_numbers.cs
@@ -0,0 +1,35 @@
+using FluentAssertions;
+using Machine.Specifications;
+
+namespace Kata.Spec
+{
+    public class when_receiving_an_empty_string
+    {
+        Establish _context = () =>
+        {
+            _systemUnderTest = new Calculator();
+        };
+
+        Because of = () => { _result = _systemUnderTest.Add(""); };
+
+        It should_return_zero = () => { _result.Should().Be(0); };
+        
+        private static Calculator _systemUnderTest;
+        private static int _result;
+    }
+
+    public class when_receiving_one_number_as_string
+    {
+        Establish _context = () =>
+        {
+            _systemUnderTest = new Calculator();
+        };
+
+        Because of = () => { _result = _systemUnderTest.Add("1"); };
+
+        It should_return_the_same_number = () => { _result.Should().Be(1); };
+        
+        private static Calculator _systemUnderTest;
+        private static int _result;
+    }
+}
---
+++ src/Kata/Calculator.cs
@@ -0,0 +1,10 @@
+namespace Kata
+{
+    public class Calculator
+    {
+        public int Add(string input)
+        {
+            return 0;
+        }
+    }
+}
