5e4256f673659a4e0d96d857e279f17ca9d19d90
Juan Carlos Calix <31570936+JuanCarlosSoftwareDeveloper@users.noreply.github.com>
2019-09-06 13:51:27 -0700

    create  test for Calculator

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.Design;
+
 namespace Kata
 {
     public class Calculator
     {
         public int Add(string userInput)
         {
+            if (!string.IsNullOrEmpty(userInput))
+            {
+                return int.Parse(userInput);
+            }
+
             return 0;
         }
+        
+        
     }
 }
